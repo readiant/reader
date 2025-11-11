583fedb0906e4b7037d732789410dad57e2c7424
GitLab CI <gitlabrunner@readiant.com>
2025-11-11 10:48:31 +0000

    Update main from GitLab CI Tue Nov 11 10:48:31 UTC 2025

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,3 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./jsx.d.ts" />
+/**
+ * First load all polyfills.
+ */
 import startReadiant from './readiant.js';
 import { registerServiceWorker } from './serviceworker.js';
 registerServiceWorker();
