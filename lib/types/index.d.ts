28580e94f833ea1d085a57f52d962b9bbf284634
GitLab CI <gitlabrunner@readiant.com>
2025-11-11 10:22:49 +0000

    Update main from GitLab CI Tue Nov 11 10:22:49 UTC 2025

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1,6 +1,3 @@
-/**
- * First load all polyfills.
- */
 import startReadiant from './readiant.js';
 import { registerServiceWorker } from './serviceworker.js';
 registerServiceWorker();
@@ -9,66 +6,3 @@ startReadiant();
  * Export public API for TypeScript consumers
  */
 export { ReadiantElement } from './base.js';
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace JSX {
-    interface IntrinsicElements {
-      'readiant-reader': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      > & {
-        'document-id'?: string;
-        page?: number | string;
-        url?: string;
-        'use-signed-urls'?: boolean | string;
-        orientation?: string;
-        disable?: string;
-
-        'zoom-level'?: number | string;
-        font?: string;
-        'font-size'?: number | string;
-        'letter-spacing'?: number | string;
-        'line-height'?: number | string;
-        'word-spacing'?: number | string;
-        'screen-mode-level'?: number | string;
-        'color-blind-filter'?: string;
-        'image-quality-level'?: number | string;
-        'text-mode-level'?: number | string;
-
-        // Audio options
-        'audio-highlighting-level'?: number | string;
-        'countdown-level'?: number | string;
-        'playback-rate'?: number | string;
-        'read-stop-level'?: number | string;
-        'subtitle-font-size'?: number | string;
-        'subtitle-level'?: number | string;
-
-        onDocumentLoaded?: (
-          event: CustomEvent<{
-            documentId: string;
-            totalPages: number;
-            isReady: boolean;
-          }>,
-        ) => void;
-        onPageChanged?: (
-          event: CustomEvent<{
-            page: number;
-            currentPage: number;
-            totalPages: number;
-            direction?: string;
-          }>,
-        ) => void;
-        onZoomChanged?: (
-          event: CustomEvent<{ zoom: number; level: number }>,
-        ) => void;
-        onThemeChanged?: (
-          event: CustomEvent<{ theme: number; level: number }>,
-        ) => void;
-        onError?: (
-          event: CustomEvent<{ message: string; type: string }>,
-        ) => void;
-      };
-    }
-  }
-}
---
+++ lib/types/jsx.d.d.ts
@@ -0,0 +1,62 @@
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'readiant-reader': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        'document-id'?: string;
+        page?: number | string;
+        url?: string;
+        'use-signed-urls'?: boolean | string;
+        orientation?: string;
+        disable?: string;
+
+        'zoom-level'?: number | string;
+        font?: string;
+        'font-size'?: number | string;
+        'letter-spacing'?: number | string;
+        'line-height'?: number | string;
+        'word-spacing'?: number | string;
+        'screen-mode-level'?: number | string;
+        'color-blind-filter'?: string;
+        'image-quality-level'?: number | string;
+        'text-mode-level'?: number | string;
+
+        'audio-highlighting-level'?: number | string;
+        'countdown-level'?: number | string;
+        'playback-rate'?: number | string;
+        'read-stop-level'?: number | string;
+        'subtitle-font-size'?: number | string;
+        'subtitle-level'?: number | string;
+
+        onDocumentLoaded?: (
+          event: CustomEvent<{
+            documentId: string;
+            totalPages: number;
+            isReady: boolean;
+          }>,
+        ) => void;
+        onPageChanged?: (
+          event: CustomEvent<{
+            page: number;
+            currentPage: number;
+            totalPages: number;
+            direction?: string;
+          }>,
+        ) => void;
+        onZoomChanged?: (
+          event: CustomEvent<{ zoom: number; level: number }>,
+        ) => void;
+        onThemeChanged?: (
+          event: CustomEvent<{ theme: number; level: number }>,
+        ) => void;
+        onError?: (
+          event: CustomEvent<{ message: string; type: string }>,
+        ) => void;
+      };
+    }
+  }
+}
+
+export {};
